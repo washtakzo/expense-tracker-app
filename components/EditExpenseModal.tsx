f310192f911c1f0014dddbc83988f2efcffbfb25
washtakzo <washtakzo@gmail.com>
2023-05-05 08:01:49 +0200

    UI : completed

--- components/EditExpenseModal.tsx
+++ components/EditExpenseModal.tsx
@@ -5,9 +5,12 @@ import {
   Button,
   Modal,
   Dimensions,
+  Pressable,
 } from "react-native";
 import React from "react";
 import { themeColors } from "../utils/colors";
+import { Button as ModalButton } from "./Button";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {
   isVisible: boolean;
@@ -24,8 +27,29 @@ const EditExpenseModal = ({ isVisible, closeModal }: Props) => {
     >
       <View style={styles.emptySpace}></View>
       <View style={styles.container}>
-        <Text>TEST</Text>
-        <Button title="Close" onPress={closeModal} />
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Edit Expense</Text>
+        </View>
+        <View style={styles.userChoicesContainer}>
+          <View style={styles.buttonsContainer}>
+            <ModalButton
+              containerStyle={styles.cancelButtonContainer}
+              textStyle={styles.cancelButtonText}
+              onPress={closeModal}
+            >
+              Cancel
+            </ModalButton>
+            <ModalButton onPress={closeModal}>Update</ModalButton>
+          </View>
+          <View style={styles.divider}></View>
+          <Pressable onPress={closeModal}>
+            <Ionicons
+              name="trash"
+              size={deviceWidth / 12}
+              color={themeColors.thirdDark}
+            />
+          </Pressable>
+        </View>
       </View>
     </Modal>
   );
@@ -44,9 +68,41 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 80,
+    backgroundColor: themeColors.primaryDark,
+    borderTopLeftRadius: deviceWidth / 36,
+    borderTopRightRadius: deviceWidth / 36,
+  },
+  titleContainer: {
     backgroundColor: themeColors.secondaryDark,
     borderTopLeftRadius: deviceWidth / 36,
     borderTopRightRadius: deviceWidth / 36,
-    padding: deviceWidth / 24,
+    justifyContent: "center",
+    alignItems: "center",
+    height: deviceWidth / 12,
+  },
+  title: {
+    color: themeColors.textOnSecondary,
+    fontWeight: "bold",
+  },
+  userChoicesContainer: {
+    paddingVertical: deviceWidth / 24,
+    paddingHorizontal: deviceWidth / 12,
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cancelButtonContainer: {
+    backgroundColor: "#ffeeff0",
+  },
+  cancelButtonText: {
+    fontWeight: "normal",
+  },
+  divider: {
+    backgroundColor: themeColors.textOnPrimary,
+    height: deviceWidth / 450,
+    width: "100%",
+    marginVertical: deviceWidth / 24,
   },
 });
